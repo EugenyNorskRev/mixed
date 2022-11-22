95c4aa1d0a93907a264a2ef529f9eead5cdef398
rev <rev@localhost.localdomain>
2022-11-22 15:03:02 +0300

    final commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+// использование смешанных выражений
+using namespace std;
+
+int main()
+{
+    int count = 10;
+    float avgWeight = 155.5F;
+    double totalWeight = count * avgWeight;
+    cout << "Вес равен " << totalWeight << endl;
+}
